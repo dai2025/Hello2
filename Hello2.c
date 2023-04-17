f0e7f9e441ae6d015be322ff61b147599bf927b3
dai <83387146+dai2024@users.noreply.github.com>
2023-04-17 12:25:51 +0000

    Update Hello2.c
    //

--- Hello2.c
+++ Hello2.c
@@ -1,17 +1,37 @@
+#ifndef _WIN32
+// #include <windows.h>
+// #include <strsafe.h>
+// WCHAR szBuffer[100];
+// HRESULT hr = StringCbPrintfExW(szBuffer, sizeof(szBuffer), L"", ... 
+// if (SUCCEEDED(hr))
+// wprintf(L"... : %s\n", szBuffer);
+#else
 #include <stdio.h>
 //#include <stdlib.h>
 //#include <cxxabi.h>
 
+#ifndef __cplusplus 
 #define print(x) printf(#x ": %s = %s\n", _Generic((x), \
     int: "int", \
+    long: "long", \
     double: "double", \
     char *: "char *", \
     const char*: "const char*", \
     default: "unknown"), #x)
+#endif
+
+#define type_name(x) _Generic((x), \
+int: "int", \
+long: "long", \
+double: "double", \
+char *: "char *", \
+		const char*: "const char *", \
+default: "unknown")
+#endif
 
 //#define print2(x) printf("%s = %s\n", type_name(&(x)), #x)
 
-int mainn() {
+int mainn(void) {
 return 0;
 }
 
@@ -19,18 +39,14 @@ static int sub1(void) {
 	return (0); // EXIT_SUCCESS;
 }
 
-int main() {
-//      https://en.wikipedia.org/wiki/Name_mangling
-//      const char *mangled_name = "_ZNK3MapI10StringName3RefI8GDScriptE10ComparatorIS0_E16DefaultAllocatorE3hasERKS0_";
-//      int status = -1;
-//      char *demangled_name = abi::__cxa_demangle(mangled_name, NULL, NULL, &status);
-//      printf("Demangled: %s\n", demangled_name);
-//      free(demangled_name);
+int main(void) {
 const char* mai="mainn";
 //print2(mai);
 print(mai);
 print(mainn);
 int (*func)(void) = sub1;
+//static int (*func2)(void) = mainn;
+//func2 = func;
 //print(sub1);
 return func();
 }
