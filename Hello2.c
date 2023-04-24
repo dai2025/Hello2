e15f413503c9816c1568fc81b33c9f91fbafb47e
dai2021-2025 <83387146+dai2025@users.noreply.github.com>
2023-04-24 08:20:55 +0000

    Update Hello2.c
    sample

--- Hello2.c
+++ Hello2.c
@@ -1,8 +1,55 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+/* I write sample code to demonstrate some basic assembly instructions in C.
+   I use the asm keyword to embed assembly code in C. 
+   I also use the outb instruction to write a byte to an I/O port. 
+   I include the linux/module.h and linux/kernel.h headers to use the printk function and the module interface. */
+	
 static int sub1(void) {
 	return (0); // EXIT_SUCCESS;
 }
 
 volatile int main(void) {
+    // This is a sample code that uses inline assembly in C
+    // It demonstrates some basic operations such as push, pop, mov, outb and inb
+    // It also prints a message to the kernel log
+
+    // Declare some variables and registers
+    register int a = 1;
+    register int c;
+    register int d = 0x55; // Some arbitrary value
+    register int addr = 0x378; // Some arbitrary port address
+
+    // Perform some operations using inline assembly
+    asm volatile (
+        "pushq %rax\n\t" // Push the value of rax to the stack
+        "popq %rax\n\t"  // Pop the value of rax from the stack
+    );
+
+    asm volatile (
+        "movl %1, %0\n\t" // Move the value of a to c
+        : "=r" (c)        // Output operand
+        : "r" (a)         // Input operand
+        :                 // No clobbered registers
+    );
+
+    asm volatile (
+        "outb %%al, %%dx" // Output the value of al to the port address in dx
+        :                 // No output operand
+        : "a"(d), "d"(addr) // Input operands
+        :                 // No clobbered registers
+    );
+
+    asm volatile (
+        "inb %%dx, %%al"  // Input the value from the port address in dx to al
+        : "=a"(d)         // Output operand
+        : "d"(addr)       // Input operand
+        :                 // No clobbered registers
+    );
+
+    printk("Goodbye, world!\n"); // Print a message to the kernel log
+	
 	int (*func)(void) = sub1;
 	return func();
 }
